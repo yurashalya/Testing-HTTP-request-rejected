8c01cc2f4c7c70d1414953c3ee6e40a4f8e3dbd7
Yura Shalya <shalyayura@gmail.com>
2019-01-29 02:32:55 +0200

    Delete data on the page view (Method delete)

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -22,22 +22,29 @@ class FullPost extends Component {
         }
     }
 
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then( response => {
+                console.log(response);
+            });
+    }
+
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         if (this.props.id) {
             post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
         }
         if(this.state.loadedPost) {
-        post = (
-            <div className="FullPost">
-                <h1>{this.state.loadedPost.title}</h1>
-                <p>{this.state.loadedPost.content}</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.content}</p>
+                    <div className="Edit">
+                        <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
+                    </div>
                 </div>
-            </div>
 
-        );
+            );
         }
         return post;
     }
